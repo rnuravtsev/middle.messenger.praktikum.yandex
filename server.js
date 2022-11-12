13a1081650cd3c1a03a15697a3ee9a8737d668d2
rnuravtsev <muravtsev21@gmail.com>
2022-11-12 13:17:51 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+
+const app = express();
+app.use(express.static(path.join(__dirname, './dist')))
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}!`);
+});
