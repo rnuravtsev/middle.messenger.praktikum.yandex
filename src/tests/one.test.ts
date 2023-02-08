f3eab96aa13a9c7e5bedaa0e757b777747616fa7
rnuravtsev <rnuravtsev@gmail.com>
2023-02-08 10:27:20 +0300

    tech. | add testing libraries and passed first tests

---
+++ src/tests/one.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('Typescript + Babel usage suite', () => {
+  it('should return string correctly', () => {
+    expect(1).to.eq(1)
+  })
+})
